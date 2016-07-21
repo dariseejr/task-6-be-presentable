cc04ff06f473053d5018f2a3dc5774dac98d5c65
cnpence <cnpence@live.unc.edu>
2016-07-21 12:53:19 -0400

    got rid of serve

--- serve/build-presentation.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-INPUT=$1
-TEMPLATE=template-index.html
-TRANSITION=slide
-THEME=black
-
-pandoc -t revealjs --template=$TEMPLATE -s --variable theme="$moon" --variable transition="$slide" --variable revealjs-url="./" downfall.md -o index.html
